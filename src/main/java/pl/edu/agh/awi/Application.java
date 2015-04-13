34b1456e5bbba40e88396ddc3dedeb7a52d9d6e6
franc90 <azarnowski1@gmail.com>
2015-04-13 09:42:09 +0200

    Add zone name loader

--- src/main/java/pl/edu/agh/awi/Application.java
+++ src/main/java/pl/edu/agh/awi/Application.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.awi;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,14 +9,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.edu.agh.awi.loader.StartupLoader;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application implements CommandLineRunner {
 
+    public static final String DEFAULT_ZONE = "northamerica";
     @Autowired
     private List<StartupLoader> loaders;
+
+    @Autowired
+    private HazelcastInstance hazelcast;
+
+    private IList<String> zones;
+
+    @PostConstruct
+    public void init() {
+        zones = hazelcast.getList("zones");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -22,6 +36,18 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        addZones(args);
+
         loaders.forEach(e -> e.loadData());
+    }
+
+    private void addZones(String... args) {
+        if (args.length > 0) {
+            for (String arg : args) {
+                zones.add(arg);
+            }
+        } else {
+            zones.add(DEFAULT_ZONE);
+        }
     }
 }
