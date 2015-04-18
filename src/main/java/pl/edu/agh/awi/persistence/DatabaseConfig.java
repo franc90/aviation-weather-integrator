cb459d44e583b5ab2820b9c85d3f0d859253dc18
konrad <jakubiec@student.agh.edu.pl>
2015-04-19 01:36:41 +0200

    - added configuration for remote database

--- src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
+++ src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
@@ -5,15 +5,19 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 
 @Configuration
 public class DatabaseConfig {
 
-    private static final String DB = "data/awi.db";
+    private static final String HOST = "http://178.62.52.247:7474/db/data";
+    private static final String USER = "neo4j";
+    private static final String PASSWORD = "Hard_pa$5";
 
     @Bean
     GraphDatabaseService graphDatabaseService() {
-        return new GraphDatabaseFactory().newEmbeddedDatabase(DB);
+        return new SpringCypherRestGraphDatabase(HOST, USER, PASSWORD);
     }
 
 }
