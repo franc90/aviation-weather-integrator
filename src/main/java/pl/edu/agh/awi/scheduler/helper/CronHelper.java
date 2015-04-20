b52ac199ae04cb8e6804b8521dd155ebf90a72af
franc90 <azarnowski1@gmail.com>
2015-04-20 20:01:03 +0200

    update cron

--- src/main/java/pl/edu/agh/awi/scheduler/helper/CronHelper.java
+++ src/main/java/pl/edu/agh/awi/scheduler/helper/CronHelper.java
@@ -4,9 +4,9 @@ public interface CronHelper {
 
     String TEST_CRON = "0/30 * * * * ?";
 
-    String FLIGHT_CRON = "* 0/15 * 1 * ?";
+    String FLIGHT_CRON = "* 0/15 * * * *";
 
-    String FLIGHT_DETAIL_CRON = "* 5/15 * 1 * ?";
+    String FLIGHT_DETAIL_CRON = "* 5/15 * * * *";
 
     String METAR_CRON = "* 12 */1 * * *";
 
