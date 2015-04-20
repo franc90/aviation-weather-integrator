3031c40ca211d3179b48419c8674da8731a9d8eb
konrad <jakubiec@student.agh.edu.pl>
2015-04-20 11:14:08 +0200

    Revert "- added configuration for remote database"
    This reverts commit cb459d44e583b5ab2820b9c85d3f0d859253dc18.

--- src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
+++ src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
@@ -5,19 +5,15 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
 
 @Configuration
 public class DatabaseConfig {
 
-    private static final String HOST = "http://178.62.52.247:7474/db/data";
-    private static final String USER = "neo4j";
-    private static final String PASSWORD = "Hard_pa$5";
+    private static final String DB = "data/awi.db";
 
     @Bean
     GraphDatabaseService graphDatabaseService() {
-        return new SpringCypherRestGraphDatabase(HOST, USER, PASSWORD);
+        return new GraphDatabaseFactory().newEmbeddedDatabase(DB);
     }
 
 }
