40728737737dc51ac4d8ecf4ce9b17b4580679c3
konrad <jakubiec@student.agh.edu.pl>
2015-04-20 19:50:34 +0200

    renamed data dir

--- src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
+++ src/main/java/pl/edu/agh/awi/persistence/DatabaseConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseConfig {
 
-    private static final String DB = "data/awi.db";
+    private static final String DB = "data/awi";
 
     @Bean
     GraphDatabaseService graphDatabaseService() {
