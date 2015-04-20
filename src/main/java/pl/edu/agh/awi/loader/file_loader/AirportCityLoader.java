cc36da80d744fd196949dcf8deffe1c79fa26f3f
franc90 <azarnowski1@gmail.com>
2015-04-20 22:31:46 +0200

    replace FileUtils with IOUtils

--- src/main/java/pl/edu/agh/awi/loader/file_loader/AirportCityLoader.java
+++ src/main/java/pl/edu/agh/awi/loader/file_loader/AirportCityLoader.java
@@ -1,24 +1,22 @@
 package pl.edu.agh.awi.loader.file_loader;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class AirportCityLoader implements FileLoader<String> {
 
-    private static final String RUNWAYS_CSV = "/airport/airportCities.csv";
+    private static final String RUNWAYS_CSV = "airport/airportCities.csv";
     private static final String NO_DATA = "";
     private static final String SEPARATOR = ",";
-    private static final String ENCODING = "UTF-8";
 
     private Map<String, String> cities;
 
@@ -46,10 +44,10 @@ public class AirportCityLoader implements FileLoader<String> {
 
     private void loadRunways() {
         try {
-            File file = FileUtils.toFile(getClass().getResource(RUNWAYS_CSV));
-            String read = FileUtils.readFileToString(file, ENCODING);
+            List<String> read = IOUtils.readLines(ClassLoader.getSystemResourceAsStream(RUNWAYS_CSV));
 
-            cities = Stream.of(read.split(System.lineSeparator()))
+            cities = read
+                    .stream()
                     .filter(e -> e.split(SEPARATOR).length == 2)
                     .collect(Collectors.toMap(
                                     (String e) -> e.split(SEPARATOR)[0],
